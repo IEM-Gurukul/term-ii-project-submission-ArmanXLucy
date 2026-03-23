f45c27ef2a1d343fcfa7f597a0212b0ea7fe6e40
ArmanXLucy <arman2klo1967@gmail.com>
2026-03-23 18:14:41 +0530

    Create diff_treat.java
    second commit on diff_treatment

---
+++ src/diff_treat.java
@@ -0,0 +1,23 @@
+class Treatment{
+public void treat(patient p){
+System.out.println("General treatment for " + p.getName());
+}
+}
+
+class FirstAid extends Treatment{
+public void treat(patient p){
+System.out.println("Providing first aid to " + p.getName());
+}
+}
+
+class Surgery extends Treatment{
+public void treat(patient p){
+System.out.println("Performing surgery for " + p.getName());
+}
+}
+
+class Medicine extends Treatment{
+public void treat(patient p){
+System.out.println("Giving medicine to " + p.getName());
+}
+}
