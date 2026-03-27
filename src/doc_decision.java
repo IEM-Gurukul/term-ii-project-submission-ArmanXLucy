5bb7f2cb9b82d7bcffe210ad63127a9f68be23c3
ArmanXLucy <arman2klo1967@gmail.com>
2026-03-27 23:39:10 +0530

    Update doc_decision.java
    getCondition() changed to getCond()

--- src/doc_decision.java
+++ src/doc_decision.java
@@ -1,20 +1,21 @@
 class Doctor {
 
-    public void treatPatient(Patient p) {
+    public void treatPatient(patient p) {
 
-        if(p.getCondition().equals("Fever")) {
+        if(p.getCond().equals("Fever")) {
             Treatment t = new Medicine();
             t.treat(p);
         }
 
-        else if(p.getCondition().equals("Injury")) {
+        else if(p.getCond().equals("Injury")) {
             Treatment t = new FirstAid();
             t.treat(p);
         }
 
-        else if(p.getCondition().equals("Heart Attack")) {
+        else if(p.getCond().equals("Heart Attack")) {
             Treatment t = new Surgery();
             t.treat(p);
         }
     }
 }
+
